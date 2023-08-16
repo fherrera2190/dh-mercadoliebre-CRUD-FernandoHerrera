a13bcb05b7ed9d874f98a20cabf2e4b48cf7c61f
Fernando Herrera <herrera.fernando1209@gmail.com>
2023-08-16 18:02:40 -0300

    index done!

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,17 +1,30 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const productsFilePath = path.join(__dirname, "../data/productsDataBase.json");
+const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
+const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
-const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 const controller = {
-	index: (req, res) => {
-		res.render('index', { products });
-	},
-	search: (req, res) => {
-		res.render('result');
-	},
+  index: (req, res) => {
+    res.render("index", {
+      productsVisited: products.filter(
+        ({ category }) => category === "visited"
+      ),
+      productsInSale: products.filter(
+        ({ category }) => category === "in-sale"
+      ),
+      toThousand
+    });
+  },
+  search: (req, res) => {
+    const keywords = req.query.keywords.toLowerCase();
+    const results = products.filter(({ name }) => name.toLowerCase().includes(keywords));
+    return res.render("results", {
+      keywords,
+      results: (results.length !== 0 ? results : false),
+      toThousand
+    });
+  },
 };
-
 module.exports = controller;
--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -9,7 +9,7 @@ const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 const controller = {
 	// Root - Show all products
 	index: (req, res) => {
-		// Do the magic
+		res.render('products', { products, toThousand })
 	},
 
 	// Detail - Detail from one product
